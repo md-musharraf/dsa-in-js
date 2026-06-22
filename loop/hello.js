d0832f19972db6337d031e858cfc07f66bcd49d1
md musharraf <116558911+md-musharraf@users.noreply.github.com>
2026-06-22 12:14:59 +0530

    more question

--- loop/hello.js
+++ loop/hello.js
@@ -17,4 +17,36 @@ let n = parseInt(prompt("Enter a number: "));
 //   console.log(i);
 // }
 
+// print the table of a n number
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`${n} x ${i} = ${n * i}`);
+// }
+
+// print the sum of numbers from 1 to n
+// let sum = 0;
+// for (let i = 1; i <= n; i++) {
+//   sum += i;
+// }
+// console.log(`The sum of numbers from 1 to ${n} is: ${sum}`);
+
+// print the factorial of a number n
+// let factorial = 1;
+// for (let i = 1; i <= n; i++) {
+//   factorial *= i;
+// }
+// console.log(`The factorial of ${n} is: ${factorial}`);
+
+// print the sum of odd and even numbers from 1 to n
+// let oddSum = 0;
+// let evenSum = 0;
+// for (let i = 1; i <= n; i++) {
+//   if (i % 2 === 0) {
+//     evenSum += i;
+//   } else {
+//     oddSum += i;
+//   }
+// }
+// console.log(`The sum of odd numbers from 1 to ${n} is: ${oddSum}`);
+// console.log(`The sum of even numbers from 1 to ${n} is: ${evenSum}`);
+
 
