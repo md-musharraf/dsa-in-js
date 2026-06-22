855f2cf1e48af4019c22d5c20c917abcd06e6e0d
md musharraf <116558911+md-musharraf@users.noreply.github.com>
2026-06-22 11:59:11 +0530

    today update

---
+++ loop/hello.js
@@ -0,0 +1,20 @@
+const prompt = require("prompt-sync")();
+
+let n = parseInt(prompt("Enter a number: "));
+
+// print "Hello, World!" n times
+// for (let i = 1; i <= n; i++) {
+//   console.log(`Hello, World! ${i}`);
+// }
+
+// print numbers from 1 to n
+// for (let i = 1; i <= n; i++) {
+//   console.log(i);
+// }
+
+// print numbers from n to 1
+// for (let i = n; i >= 1; i--) {
+//   console.log(i);
+// }
+
+
