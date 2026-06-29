477e02e2a216c6262150d378d198a73fcd6e50a4
md musharraf <116558911+md-musharraf@users.noreply.github.com>
2026-06-29 16:38:42 +0530

    second problem

--- pattern_programing/pattern.js
+++ pattern_programing/pattern.js
@@ -2,9 +2,28 @@ const prompt = require("prompt-sync")();
 
 let n = Number(prompt("Enter a number :- "));
 
-for (let i = 1; i <= n; i++) {
-  for (let j = 1; j <= i; j++) {
-    process.stdout.write("* ");
-  }
-  console.log();
-}
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= i; j++) {
+//     process.stdout.write("* ");
+//   }
+//   console.log();
+// }
+// output -
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= i; j++) {
+//     process.stdout.write(`${j} `);
+//   }
+//   console.log();
+// }
+// output:-
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
