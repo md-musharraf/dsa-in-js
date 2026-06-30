2d6e390d2d76a81e59b2309063cf7d57a31ede84
md musharraf <116558911+md-musharraf@users.noreply.github.com>
2026-06-30 18:00:55 +0530

    add charater piramid

--- pattern_programing/pattern.js
+++ pattern_programing/pattern.js
@@ -28,4 +28,17 @@ let n = Number(prompt("Enter a number :- "));
 // 1 2 3 4
 // 1 2 3 4 5
 
-
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= i; j++) {
+//     process.stdout.write(String.fromCharCode(64 + j) + " ");
+//   }
+//   console.log();
+// }
+// output -
+// A
+// A B
+// A B C
+// A B C D
+// A B C D E
+// A B C D E F
+// A B C D E F G
