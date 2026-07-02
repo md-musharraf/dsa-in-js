0a8ac0081f0869beb817342995e228cb9507fc79
md musharraf <116558911+md-musharraf@users.noreply.github.com>
2026-07-02 16:02:24 +0530

    todays first question solve

--- pattern_programing/pattern.js
+++ pattern_programing/pattern.js
@@ -54,3 +54,19 @@ let n = Number(prompt("Enter a number :- "));
 // * * *
 // * *
 // *
+
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= n - i; j++) {
+//     process.stdout.write("  ");
+//   }
+//   for (let k = 1; k <= i; k++) {
+//     process.stdout.write("* ");
+//   }
+//   console.log();
+// }
+// output -
+//         *
+//       * *
+//     * * *
+//   * * * *
+// * * * * *
