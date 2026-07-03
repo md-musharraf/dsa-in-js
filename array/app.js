bbff27449e975c4e06b54f3f5825f0aefe340831
md musharraf <116558911+md-musharraf@users.noreply.github.com>
2026-07-03 16:42:18 +0530

    today we start array in js

---
+++ array/app.js
@@ -0,0 +1,9 @@
+const arr = [10, 20, 20, 40, 3, 0, 99];
+
+// let max = arr[0];
+// for (let i = 0; i < arr.length; i++) {
+//   if (max < arr[i]) {
+//     max = arr[i];
+//   }
+// }
+// console.log(max);
