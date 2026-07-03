7a0c569d56bffc018e85e872ac2f9eecd9b2172b
md musharraf <116558911+md-musharraf@users.noreply.github.com>
2026-07-03 17:05:03 +0530

    fix index in array

--- array/app.js
+++ array/app.js
@@ -1,5 +1,4 @@
-const arr = [10, 20, 20, 40, 3, 0, 99];
-
+// const arr = [10, 20, 20, 40, 3, 0, 99];
 // let max = arr[0];
 // for (let i = 0; i < arr.length; i++) {
 //   if (max < arr[i]) {
@@ -7,3 +6,12 @@ const arr = [10, 20, 20, 40, 3, 0, 99];
 //   }
 // }
 // console.log(max);
+
+// find the index
+// const arr = [10, 25, 30, 45, 60];
+// let target = 45;
+// for (let i = 0; i < arr.length; i++) {
+//   if (target === arr[i]) {
+//     console.log(i);
+//   }
+// }
