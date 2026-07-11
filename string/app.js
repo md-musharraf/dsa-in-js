630819a4003bc0db2a0eb9620c00efd419ba3d36
md musharraf <116558911+md-musharraf@users.noreply.github.com>
2026-07-11 17:08:32 +0530

    palidrome in string

--- string/app.js
+++ string/app.js
@@ -1,6 +1,20 @@
-let str = "hello";
-let revStr = "";
-for (let i = str.length - 1; i >= 0; i--) {
-  revStr = revStr + str[i];
-}
-console.log(revStr);
+// let str = "hello";
+// let revStr = "";
+// for (let i = str.length - 1; i >= 0; i--) {
+//   revStr = revStr + str[i];
+// }
+// console.log(revStr);
+
+// let str = "madammam";
+// let palindrome = true;
+// for (let i = 0; i < Math.floor(str.length / 2); i++) {
+//   if (str[i] !== str[str.length - 1 - i]) {
+//     palindrome = false;
+//     break;
+//   }
+// }
+// if (palindrome) {
+//   console.log("This is a palindrome string");
+// } else {
+//   console.log("This is not a palindrome string");
+// }
