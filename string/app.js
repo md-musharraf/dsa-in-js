a8993a2dd4b2da704922b82346789335fd343ff5
Md Musharraf <mdmusharraf41125@gmail.com>
2026-07-13 16:25:21 +0530

    find the digit

--- string/app.js
+++ string/app.js
@@ -56,3 +56,13 @@
 //   }
 // }
 // console.log(uppercase);
+
+// // find the digit in this word
+// let str = "abc123xyz45";
+// let count = 0;
+// for (let i = 0; i < str.length; i++) {
+//   if (str[i] >= "0" && str[i] <= "9") {
+//     count++;
+//   }
+// }
+// console.log(count);
